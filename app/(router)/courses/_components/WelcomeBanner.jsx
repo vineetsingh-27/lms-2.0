025cd6b7c98502b156a3ffb891885391d4fa64f7
Vineet <vineet199927@gmail.com>
2024-05-27 13:12:09 +0530

    Till Sidebar and Header completed.

---
+++ app/(router)/courses/_components/WelcomeBanner.jsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+function WelcomeBanner() {
+  return (
+    <div className='flex gap-5 items-center bg-white rounded-xl p-5'>
+        <Image src="/panda.jpg"
+        width={100}
+        height={100} />
+        <div>
+            <h2 className='font-bold text-[29px]'>Welcome to <span className='text-primary'>TubeGuruji</span> Academy</h2>
+            <h2 className='text-gray-500'>Explore, Learn and Build All Life Projects</h2>
+        </div>
+    </div>
+  )
+}
+
+export default WelcomeBanner
---
+++ app/(router)/courses/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import WelcomeBanner from './_components/WelcomeBanner'
+
+function Courses() {
+  return (
+    <div className='grid sm:grid-cols-1 md:grid-cols-3 p-5'>
+      {/* Left Container */}
+      <div className='col-span-2'>
+        {/* Banner */}
+        <WelcomeBanner/>
+      </div>
+      {/* Right Container */}
+      <div>
+        Right Section
+      </div>
+    </div>
+  )
+}
+
+export default Courses
